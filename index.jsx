daf37eb607a94498714a9a6eaf30334b10e92842
X-Edition <abcdefghij151515hi@gmail.com>
2024-12-02 09:54:40 -0600

    Updated!

---
+++ index.jsx
@@ -0,0 +1,46 @@
+<div style={{width: 440, height: 956, position: 'relative', background: '#ACACAC'}}>
+  <div style={{width: 440, height: 99, left: 0, top: 857, position: 'absolute'}}>
+    <div style={{width: 99, height: 440, left: 0, top: 99, position: 'absolute', transform: 'rotate(-90deg)', transformOrigin: '0 0', background: '#D9D9D9', borderRight: '2px black solid'}} />
+    <div style={{width: 78, height: 78, left: 337, top: 13, position: 'absolute'}}>
+      <div style={{width: 78, height: 78, left: 0, top: 0, position: 'absolute', background: '#D9D9D9', borderRadius: 10, border: '2px black solid'}} />
+      <div style={{width: 48, height: 48, paddingTop: 2.78, paddingBottom: 4, paddingLeft: 4, paddingRight: 5.12, left: 15, top: 15, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 38.88, height: 41.22, border: '4px black solid'}}></div>
+      </div>
+    </div>
+    <div style={{width: 78, height: 78, left: 233, top: 13, position: 'absolute'}}>
+      <div style={{width: 78, height: 78, left: 0, top: 0, position: 'absolute', background: '#D9D9D9', borderRadius: 10, border: '2px black solid'}} />
+      <div style={{width: 52, height: 52, padding: 10.83, left: 13, top: 13, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 30.33, height: 30.33, background: 'black'}}></div>
+      </div>
+    </div>
+    <div style={{width: 78, height: 78, left: 128, top: 13, position: 'absolute'}}>
+      <div style={{width: 78, height: 78, left: 0, top: 0, position: 'absolute', background: '#D9D9D9', borderRadius: 10, border: '2px black solid'}} />
+      <div style={{width: 48, height: 48, paddingTop: 5.76, paddingBottom: 8, paddingLeft: 6, paddingRight: 6, left: 15, top: 15, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 36, height: 34.24, border: '4px black solid'}}></div>
+      </div>
+    </div>
+    <div style={{width: 78, height: 78, left: 24, top: 13, position: 'absolute'}}>
+      <div style={{width: 78, height: 78, left: 0, top: 0, position: 'absolute', background: '#73DEFF', borderRadius: 10, border: '4px black solid'}} />
+      <div style={{width: 48, height: 48, paddingLeft: 6, paddingRight: 6, paddingTop: 4, paddingBottom: 4, left: 15, top: 15, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 36, height: 40, border: '4px black solid'}}></div>
+      </div>
+    </div>
+  </div>
+  <div style={{width: 420, height: 329, left: 10, top: 511, position: 'absolute'}}>
+    <div style={{width: 420, height: 329, left: 0, top: 0, position: 'absolute', background: '#737EFF', borderRadius: 10, border: '1px black solid'}} />
+    <div style={{left: 19, top: 17, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>To Do:</div>
+  </div>
+  <div style={{width: 420, height: 329, left: 10, top: 169, position: 'absolute'}}>
+    <div style={{width: 420, height: 329, left: 0, top: 0, position: 'absolute', background: '#FF7375', borderRadius: 10, border: '1px black solid'}} />
+    <div style={{left: 19, top: 17, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Notifications:</div>
+  </div>
+  <div style={{left: 106, top: 98, position: 'absolute', color: 'white', fontSize: 50, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Welcome</div>
+  <div style={{width: 440, height: 83, left: 0, top: 0, position: 'absolute'}}>
+    <div style={{width: 440, height: 83, left: 0, top: 0, position: 'absolute', background: '#73DEFF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderBottom: '2px black solid'}} />
+    <div style={{width: 221, height: 43, left: 24, top: 29, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>iApply</div>
+    <div style={{width: 89, height: 55, left: 342, top: 10, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>JDubbbz</div>
+    <div style={{width: 24, height: 24, padding: 2, left: 313, top: 10, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div style={{width: 20, height: 20, background: '#1D1B20'}}></div>
+    </div>
+  </div>
+</div>
